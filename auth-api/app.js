46ad53678c732a62418f916ebd95eb3f0f5ff7ef
R. Alejandro Fuentes <fuentesra@hotmail.com>
2023-07-24 14:36:11 -0300

    feat - auth - add request root status 200

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,6 +1,6 @@
-import express  from "express";
+import express from "express";
 
-import userRouter from './src/modules/user/routes/UserRoutes.js';
+import userRouter from "./src/modules/user/routes/UserRoutes.js";
 import * as db from "./src/config/db/initialData.js";
 import trancing from "./src/config/trancing.js";
 
@@ -9,37 +9,45 @@ const env = process.env;
 const PORT = env.PORT || 8080;
 const CONTAINER_ENV = "container";
 
-
-//help check - only know if our service is up
-app.get('/api/status', (req, res) => {
-    return res
-    .status(200)
-    .json({
-        service: 'AUTH API',
-        httpStatus: 200,
-        status: 'UP',
-    })
-})
-
 app.use(express.json());
 startApplication();
 createDataForTesting();
+getStatusApi();
 app.use(trancing);
 app.use(userRouter);
 
 app.listen(PORT, () => {
-    console.info(`Server started in port ${PORT}`);
-})
+  console.info(`Server started in port ${PORT}`);
+});
 
-function startApplication(){
-    if(env.NODE_ENV !== CONTAINER_ENV){
-        db.createInitialData();
-    }
+function getStatusApi() {
+  //help check - only know if our service is up
+  app.get("/", (req, res) => {
+    return res.status(200).json(getOkResponse());
+  });
+  // root check - for AWS checking to healft or service.
+  app.get("/api/status", (req, res) => {
+    return res.status(200).json(getOkResponse());
+  });
 }
 
-function createDataForTesting(){
-    app.get("/api/initial-data", (req, res)=>{
-        db.createInitialData();
-        return res.json({message: "Data for testing created."});
-    });
+function getOkResponse() {
+  return {
+    service: "AUTH API",
+    httpStatus: 200,
+    status: "UP",
+  };
+}
+
+function startApplication() {
+  if (env.NODE_ENV !== CONTAINER_ENV) {
+    db.createInitialData();
+  }
+}
+
+function createDataForTesting() {
+  app.get("/api/initial-data", (req, res) => {
+    db.createInitialData();
+    return res.json({ message: "Data for testing created." });
+  });
 }
